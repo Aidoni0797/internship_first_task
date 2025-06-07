c0b081286c7b36310e59aba9d5fcd0d3a7c3e5d8
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-06-07 16:01:58 +0600

    task_62.py

---
+++ task_62.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Chrome() as browser:
+  browser.get('http://parsinger.ru/blank/modal/1/index.html')
+  browser.find_element(By.ID, 'confirm').click()
+  time.sleep(2)
+  prompt = browser.switch_to.alert
+  prompt .accept() #Замените на .dismiss() чтобы нажать на кнопку "Отмена"
+  time.sleep(.5)
