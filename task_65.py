3936ff2d69d4f36b4f96512c99f5a0292174f2d3
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-06-08 10:32:46 +0600

    task_65.py

---
+++ task_65.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+with webdriver.Chrome() as browser:
+  browser.get('http://parsinger.ru/window_size/1/')
+  browser.set_window_size(1200, 720)
+  print(browser.get_window_size())
